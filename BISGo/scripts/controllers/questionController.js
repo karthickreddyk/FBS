a102acad4a57ada59e52f13ec33a6eda0213c1f7
karthickreddyk <karthickreddyk@gmail.com>
2017-06-15 18:54:19 +0530

    Working on the Nested Div / Tables & Sub Array for Sub Questions

---
+++ BISGo/scripts/controllers/questionController.js
@@ -0,0 +1,30 @@
+﻿(function () {
+
+    var questionController = function ($scope) {
+        $scope.message = "Hello from questionController";
+
+        $scope.Question = [];
+
+        $scope.mainQuestion = [
+            "Main Question 1",
+            "Main Question 1",
+            "Main Question 1",
+            "Main Question 1",
+            "Main Question 1"
+        ];
+
+
+        $scope.fetchSubQuestion = function () {
+            $scope.books = [
+                { ID: 1, Question: "Sub Question 1", Desc: "Sub Question Desc 1"},
+                { ID: 2, Question: "Sub Question 2", Desc: "Sub Question Desc 2"},
+                { ID: 3, Question: "Sub Question 3", Desc: "Sub Question Desc 3"},
+                { ID: 4, Question: "Sub Question 4", Desc: "Sub Question Desc 4"},
+                { ID: 5, Question: "Sub Question 5", Desc: "Sub Question Desc 5"}
+            ];
+        }
+    }
+
+    angular.module('myApp').controller('questionController', ["$scope", questionController]);
+
+}());
